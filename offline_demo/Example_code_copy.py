1767efbe7837aad9e82e20a231cebabd75ef6ca5
sirsir23333 <your_email@youremail.com>
2024-03-16 18:08:45 +0800

    now Example code can calculate score, and Example_code_copy is my first try to find the null space

--- offline_demo/Example_code_copy.py
+++ offline_demo/Example_code_copy.py
@@ -80,6 +80,23 @@ def read_blackbox(input_data):
     n = n.reshape(300,32) # This step is to reshape Y to a matrix
     n = n.T # This step is to match the size of Y in the document
     return n
+def find_row_ratio():
+    matrix_10= np.mat(np.zeros((32, 32))) + 1j * np.mat(np.zeros((32, 32)))
+    matrix_10[0,0] = 1.0 + 0.0*1j
+    matrix_10_input = mtx2outputdata(matrix_10)
+    y_10 = blk.blackboxSystem(matrix_10_input, matrix_10_input)
+    y_10 = read_blackbox(y_10)
+    ratio_matrix = np.mat(np.zeros((32, 300), dtype=complex))
+    ratio_matrix[0] = (y_10 / y_10)[0]
+    
+    for i in range(1, 32):
+        matrix_01 = np.mat(np.zeros((32, 32))) + 1j * np.mat(np.zeros((32, 32)))
+        matrix_01[0,i] = 1.0 + 0.0*1j
+        matrix_01_input = mtx2outputdata(matrix_01)
+        y_01 = blk.blackboxSystem(matrix_01_input, matrix_01_input)
+        y_01 = read_blackbox(y_01)
+        ratio_matrix[i] = (y_01 / y_10)[0]
+    return ratio_matrix
 # ---Step 1: Input 'Start'---
 line1 = stdin.readline().strip()
 
@@ -127,7 +144,6 @@ for i in range(1, 32):
     Y01 = read_blackbox(rece_Y1)
     if test_large(Y01[i], order=test_order):
         h_idx.append(i)
-        
     else: #not interference
         # print('haha')
         if N_tar != -1:
@@ -145,7 +161,6 @@ for i in range(1, 32):
             rank = np.sum(S > tolerance)
             N_tar = rank    
 if N_tar == -1: #means case 3 case 4
-    
     for i in range(1, 32):
         W1 = np.mat(np.zeros((32, 32))) + 1j * np.mat(np.zeros((32, 32)))
         W1[i,0] = 1.0 + 0.0*1j
@@ -172,14 +187,49 @@ if N_tar == -1: #means case 3 case 4
             U, S, Vh = np.linalg.svd(Y01)
             tolerance = 1e-5 
             rank = np.sum(S > tolerance)
-            N_tar = rank    
-            
-            
+            N_tar = rank            
 # N_target can be [1,10]
 N_target = N_tar # Notices that the N_target is different for each case. Contestants program need to estimate the number of target signals first.
 
-L_est = np.mat((np.random.randn(N_target,32) + 1j*np.random.randn(N_target,32)))
+rand_time = 50
+np.random.seed(4518)
+y_first_cols = []
+W_matrices = []  # Store all W1 matrices
+null = np.mat(np.zeros((N_target, 32))) + 1j * np.mat(np.zeros((N_target, 32)))
 
+for j in range(rand_time):
+    W1 = np.mat(np.zeros((32, 32))) + 1j * np.mat(np.zeros((32, 32)))
+    W1[:, 0] = np.random.rand(32, 1) + 1j * np.random.rand(32, 1)
+    W1 = bf_norm_multiple_stream(W1)
+    W2 = np.mat(np.zeros((32, 32))) + 1j * np.mat(np.zeros((32, 32)))
+    W2[0, 0] = 1.0 +  0.0*1j
+    W2 = bf_norm_multiple_stream(W2)
+    # Store the matrix for later retrieval
+    W_matrices.append(W1)
+    # Output W1 W2
+    input_01 = mtx2outputdata(W1)
+    input_02 = mtx2outputdata(W2)
+    rece_Y1 = blk.blackboxSystem(input_01, input_02)    
+    Y01 = read_blackbox(rece_Y1)
+    for inter_idx in h_idx:  #delete the interference rows
+        Y01[inter_idx, :] = 0
+    # print("current Y01[inter_idx]= ", Y01[inter_idx])
+    # Store the norm and the index
+    y_first_cols.append((np.linalg.norm(Y01[:, 0]), j))
+    # Sort by the norm and get indices of the smallest norms
+y_first_cols_sort = sorted(y_first_cols, key=lambda x: x[0])
+print('smallest_norm=',y_first_cols_sort)
+chosen_indices = [idx for norm, idx in y_first_cols_sort[:N_target]]
+# Retrieve corresponding W1 matrices
+chosen_W_matrices = [np.array(W_matrices[idx][:,0]) for idx in chosen_indices]
+print("length of chosen_W_matrices=", len(chosen_W_matrices))
+ratio_matrix = find_row_ratio()
+for k in range(len(chosen_W_matrices)):
+    null_col = np.mat(np.array(ratio_matrix[:,0]) * chosen_W_matrices[k]).T
+    null[k,:] = null_col
+# print(null)
+# L_est = np.mat((np.random.randn(N_target,32) + 1j*np.random.randn(N_target,32)))
+L_est = null
 # L_est Normalization
 L_est = norm_multiple_stream_result(L_est)
 #******Example code end*******
@@ -195,7 +245,7 @@ line2 = stdin.readline().strip()
 
 # Output L_est
 L_est_str=  mtx2outputdata_result(L_est)
-print("Number of signal = ",N_tar)
+print("Number of signal = ",N_target)
 print(h_idx)
 print("number of interference =",len(h_idx))
 
